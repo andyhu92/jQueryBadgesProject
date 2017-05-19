e8dffdea20256dcb629f655eb1c450545ece5a00
CihangHu <alreadyusedagain@gmail.com>
2017-05-18 20:59:55 -0400

    First Commit

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,22 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: "https://www.codeschool.com/users/rush2345.json",
+    dataType: "jsonp",
+    success: function(response){
+      var courses = response.courses.completed;
+      courses.forEach(function(course) {
+         var div = "<div class='course'>" + 
+         "<h3>" + course.title + "</h3>" +
+         "<img src='" + course.badge + "'/>" + 
+         "<a class='btn btn-primary' target='_blank' href='" + course.url + "'>See Course</a>" + 
+         "</div>";
+         
+         $("#badges").append(div);
+      });
+
+    }
+  })
 
 });
